242912b686b3a9af11f64471cbc7d90f615c0a63
amanverma-1001 <ehaan1026@gmail.com>
2021-01-28 13:37:55 +0530

    Add theory

---
+++ Bubblesort/bubble_sort_updated.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    $("#type1").click(function(){
+        $("#display").empty();
+        var a,tmp;
+        tmp=$("#uni").val();
+        arr=tmp.split(",");
+        a=arr.map(function(k){
+            k=Number(k);
+            return k;
+        });
+        console.log(a);
+        bubblesort(a,0);
+        
+        
+           
+       
+        function bubblesort(a,i){
+            let t=0;
+            if(i==a.length)
+            {
+                return;
+            }
+            else{
+              for(j=0;j<a.length-i;j++)
+              {
+                  if(a[j]>a[j+1])
+                  {
+                      beforeappend(a,a[j],a[j+1]);
+                      t=a[j];
+                      a[j]=a[j+1];
+                      a[j+1]=t;
+                  }
+              }
+                
+                console.log(a);
+               setTimeout(function(){
+                bubblesort(a,i+1);
+            },2000);
+        }}
+ 
+ 
+        function beforeappend(arr,c,d)
+        {
+            var x="",e="";
+         for(i=0;i<arr.length;i++)
+         {
+             if(arr[i]==c)
+             {
+         x+=`<div class="p-2 bg-info" style="width:50px;position:relative">${arr[i]}</div>`;
+             }
+             else if(arr[i]==d)
+             {
+                x+=`<div class="p-2 bg-info" style="width:50px;position:relative">${arr[i]}</div>`;
+ 
+             }
+             else
+             {
+                 x+=`<div class="p-2 bg-warning" style="width:50px">${arr[i]}</div>`;
+ 
+             }
+         }
+         e=`<div class="d-flex p-3 bg-secondary text-white">${x}</div>`;
+         $("#display").append(e);
+         var m=$("#display div:last-child div");
+         for(k=0;k<arr.length;k++)
+         {
+             if(arr[k]==c)
+             {
+                $(m[k]).animate({
+               left:'50px' });
+                }
+               if(arr[k]==d)
+                {
+                   $(m[k]).animate({
+                       left:'-50px'
+                   });
+        
+                } 
+                }
+            }
+
+      
+    
+    });
+ });
